58e00bc5b2b4e1b81a27d39b4fa5f5fc625884af
taekgil99 <pjwlover2005@naver.com>
2023-01-02 21:42:18 +0900

    server Initial commit

---
+++ server/src/main/java/com/omc/OmcApplication.java
@@ -0,0 +1,13 @@
+package com.omc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmcApplication.class, args);
+	}
+
+}
