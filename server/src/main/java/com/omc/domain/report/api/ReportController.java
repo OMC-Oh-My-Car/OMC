dd60fb3e9456a13200f342bfdf77167c1e7cb12a
taekgil99 <pjwlover2005@naver.com>
2023-01-02 22:57:44 +0900

    Feat: init Report #2

---
+++ server/src/main/java/com/omc/domain/report/api/ReportController.java
@@ -0,0 +1,7 @@
+package com.omc.domain.report.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+}
---
+++ server/src/main/java/com/omc/domain/report/dto/ReportDto.java
@@ -0,0 +1,4 @@
+package com.omc.domain.report.dto;
+
+public class ReportDto {
+}
