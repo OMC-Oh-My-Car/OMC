20d2c162d5c556cf0aaf1fc8f362be094805ad79
shinker1002 <shinker1002@naver.com>
2023-01-04 20:59:25 +0900

    Chore: export 구조 변경

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 // import React from 'react';
 
-export default function LandingPage() {
+function LandingPage() {
   return (
     <>
       <div>LandingPage</div>
     </>
   );
 }
+
+export default LandingPage;
