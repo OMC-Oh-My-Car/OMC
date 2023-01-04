f8198bbaf4d62f725f50376946b631e28e5f29e0
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2023-01-04 18:57:30 +0900

    Feat : header와 body에 값 추가, 유효성 체크

--- server/src/main/java/com/omc/domain/member/api/MemberController.java
+++ server/src/main/java/com/omc/domain/member/api/MemberController.java
@@ -1,9 +1,11 @@
 package com.omc.domain.member.api;
 
+import com.omc.domain.member.dto.LoginDto;
 import com.omc.domain.member.dto.MemberPostDto;
 import com.omc.domain.member.dto.MemberResponseDto;
 import com.omc.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,5 +25,19 @@ public class MemberController {
         MemberResponseDto newMember = memberService.join(memberPostDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(newMember);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.UNAUTHORIZED);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getEmail(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 }
--- server/src/main/java/com/omc/domain/member/dto/LoginDto.java
+++ server/src/main/java/com/omc/domain/member/dto/LoginDto.java
@@ -2,8 +2,16 @@ package com.omc.domain.member.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class LoginDto {
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+
+    public boolean isNotValid() {
+        return email == null || password == null || email.trim().length() == 0 || password.trim().length() == 0;
+    }
 }
