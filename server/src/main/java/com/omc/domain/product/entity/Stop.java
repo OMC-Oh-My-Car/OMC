e00c52e5539050692d6382e09cec377ff5037b86
Taekgil99 <taekgil273@gmail.com>
2023-01-05 17:43:03 +0900

    Feat: Stop 엔티티 생성 #10

---
+++ server/src/main/java/com/omc/domain/product/entity/Stop.java
@@ -0,0 +1,29 @@
+package com.omc.domain.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.omc.global.common.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Stop extends BaseEntity {
+
+	@Column
+	private Long isStop;
+
+	@Column
+	private String reason;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn
+	private Product product;
+}
