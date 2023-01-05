78f4b6d237fa36af7f19234428880ef9837e0182
shinker1002 <shinker1002@naver.com>
2023-01-05 20:24:28 +0900

    Feat: 검색 컴포넌트 구현

--- client/src/components/header/nav/searchInput/SearchInput.jsx
+++ client/src/components/header/nav/searchInput/SearchInput.jsx
@@ -1,11 +1,16 @@
 // import React from 'react';
 import { SearchInputArea } from './SearchInput.styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const SearchInput = () => {
+const SearchInput = ({ type }) => {
   return (
     <>
-      <SearchInputArea>
+      <SearchInputArea type={type}>
         <input type="text" />
+        <div className="searchIconArea">
+          <FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />
+        </div>
       </SearchInputArea>
     </>
   );
--- client/src/components/header/nav/searchInput/SearchInput.styled.js
+++ client/src/components/header/nav/searchInput/SearchInput.styled.js
@@ -1,16 +1,43 @@
 import styled from 'styled-components';
 
 export const SearchInputArea = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-grow: 1;
+  max-width: ${(props) => (props.type === 'short' ? '520px' : '670px')};
   height: 100%;
-  border: 1px solid black;
+  /* border: 1px solid black; */
   input {
     width: 100%;
     height: 100%;
-    max-width: 520px;
+    font-size: 17px;
+    font-weight: 500;
+    padding: 0.6em 1.2em;
     border-radius: 25px;
+    outline: none;
+    border: 2px solid #94989c;
+    /* box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.1), 0 1px 4px hsla(0, 0%, 0%, 0.1), 0 2px 8px hsla(0, 0%, 0%, 0.1); */
+  }
+  Input:focus {
+    border: 1px solid #53baff;
+    outline: 3px solid #b5deff;
+  }
+  .searchIconArea {
+    position: absolute;
+    right: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.red};
+    .searchIcon {
+      position: absolute;
+      color: white;
+      cursor: pointer;
+    }
   }
 `;
