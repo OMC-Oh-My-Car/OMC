e9dad6dd5a4a2482328a1d7eed83acc547e33530
Taekgil99 <taekgil273@gmail.com>
2023-01-05 18:06:08 +0900

    Feat: Location Entity, Repository 구현 #10

---
+++ server/src/main/java/com/omc/domain/product/repository/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.omc.domain.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.omc.domain.product.entity.Location;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+
+}
