3334f2db0e0c12c3430e9a04e21d7c0bbf934d02
Taekgil99 <taekgil273@gmail.com>
2023-01-05 17:27:30 +0900

    Feat: FacilitiesRepository 구현 #10

---
+++ server/src/main/java/com/omc/domain/product/repository/FacilitiesRepository.java
@@ -0,0 +1,9 @@
+package com.omc.domain.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.omc.domain.product.entity.Facilities;
+
+public interface FacilitiesRepository extends JpaRepository<Facilities, Long> {
+
+}
