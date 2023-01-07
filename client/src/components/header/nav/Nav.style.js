28b1fa9ee980dc2c34ed9fc3d59934c3c4d62f28
shinker1002 <shinker1002@naver.com>
2023-01-07 19:26:02 +0900

    Feat: 헤더 반응형 추가

--- client/src/components/header/nav/Nav.style.js
+++ client/src/components/header/nav/Nav.style.js
@@ -5,8 +5,14 @@ export const NavArea = styled.nav`
   justify-content: space-between;
   align-items: center;
   gap: 35px;
+  padding: ${(props) => (props.type === 'short' ? '10px 300px' : '10px 100px')};
   width: 100%;
   height: 100%;
-  max-width: ${(props) => (props.type === 'short' ? '1280px' : '1600px')};
-  padding: 10px;
+  @media (max-width: 1425px) {
+    padding: ${(props) => (props.type === 'short' ? '10px 300px' : '10px 50px')};
+  }
+  /*모바일 */
+  @media (max-width: 744px) {
+    padding: ${(props) => (props.type === 'short' ? '10px 300px' : '10px 25px')};
+  }
 `;
