d250287a6cb0675e9a82b6c5db526c0e74574e74
shinker1002 <shinker1002@naver.com>
2023-01-10 00:17:58 +0900

    Feat: 데이터 파일 삭제

--- client/src/components/productList/ProductList.jsx
+++ client/src/components/productList/ProductList.jsx
@@ -2,52 +2,15 @@
 import { ProductListArea } from './ProductList.style';
 import ProductItem from './productItem/ProductItem';
 
-const ProductList = () => {
-  // 임시 데이터
-  let data = [
-    {
-      productId: 1,
-      subject: '전주 한옥마을',
-      description: '상품 설명',
-      location: [{ value: '전라북도 전주시' }, { value: '마포구' }, { value: '월드컵로' }],
-      reportCount: 14,
-      price: 100000,
-      star: 3.52,
-      img: [
-        {
-          value:
-            'https://www.hyundai.co.kr/image/upload/asset_library/MDA00000000000005326/9c99a0fb7e254d338ba6d92c7eb5d874.jpg',
-        },
-        { value: 'https://dimg.donga.com/ugc/CDB/WOMAN/Article/5e/fb/e2/46/5efbe2462079d2738de6.jpg' },
-      ],
-      like: 10,
-      isLike: true,
-    },
-    {
-      productId: 2,
-      subject: '상호명',
-      description: '상품 설명',
-      location: [{ value: '서울특별시' }, { value: '마포구' }, { value: '월드컵로' }],
-      reportCount: 14,
-      price: 100000,
-      star: 3.52,
-      img: [
-        {
-          value:
-            'https://www.hyundai.co.kr/image/upload/asset_library/MDA00000000000005326/9c99a0fb7e254d338ba6d92c7eb5d874.jpg',
-        },
-        { value: 'https://dimg.donga.com/ugc/CDB/WOMAN/Article/5e/fb/e2/46/5efbe2462079d2738de6.jpg' },
-      ],
-      like: 10,
-      isLike: false,
-    },
-  ];
-
+const ProductList = ({ data, isLoading, isError }) => {
+  console.log(data);
+  console.log(isLoading);
+  console.log(isError);
   return (
     <>
       <ProductListArea>
         <div className="productList">
-          {data.map((item) => {
+          {data.data.map((item) => {
             return <ProductItem key={item.productId} item={item} />;
           })}
         </div>
