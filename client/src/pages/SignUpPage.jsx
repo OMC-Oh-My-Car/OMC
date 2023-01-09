168fefdb60b17c03e42128e848087a94c5af5e03
nahoyeon <nahy0107@naver.com>
2023-01-09 15:40:55 +0900

    Feat : 회원가입 페이지 구현 및 연결

---
+++ client/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import SignUp from '../components/signUp/SignUp';
+
+const SignUpPage = () => {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+};
+export default SignUpPage;
