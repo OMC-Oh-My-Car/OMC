b56fb698089f89abe4c464c54ad0efc1846838ab
shinker1002 <shinker1002@naver.com>
2023-01-10 00:18:33 +0900

    Feat: useQuery 적용

--- client/src/pages/ProductListPage.jsx
+++ client/src/pages/ProductListPage.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react';
+import { useQuery } from 'react-query';
 import { Container } from './ProductListPage.style';
 import Header from '../components/header/Header';
 import ProductList from '../components/productList/ProductList';
+import { getProductList } from '../modules/product/productList';
 
 const ProductListPage = () => {
   const [filter, setFilter] = useState(0);
+
+  const { isLoading, data, isError } = useQuery('productList', async () => {
+    const data = await getProductList(filter, 1, 'search');
+    return data;
+  });
   return (
     <>
       <Container>
         <Header type="long" />
-        <ProductList filter={filter} setFilter={setFilter} />
+        <div className="productFilter">
+          <button className={`productFilterItem ${filter === 0 ? ' active' : ''}`} onClick={() => setFilter(0)}>
+            최신순
+          </button>
+          <button className={`productFilterItem ${filter === 1 ? ' active' : ''}`} onClick={() => setFilter(1)}>
+            인기순
+          </button>
+          <button className={`productFilterItem ${filter === 2 ? ' active' : ''}`} onClick={() => setFilter(2)}>
+            조회순
+          </button>
+          <button className={`productFilterItem ${filter === 3 ? ' active' : ''}`} onClick={() => setFilter(3)}>
+            추천순
+          </button>
+        </div>
+        <ProductList data={data} isLoading={isLoading} isError={isError} />
       </Container>
     </>
   );
