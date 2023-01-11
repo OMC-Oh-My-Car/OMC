2c217d04915e4c749c95a5cd1a3e08fe5073c162
nahoyeon <nahy0107@naver.com>
2023-01-12 01:58:38 +0900

    feat : google clientId 추가

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { store } from './redux/Store';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,11 +23,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      </QueryClientProvider>
-    </Provider>
+    <GoogleOAuthProvider clientId="22249000667-f7k5t25nm5qobstfhbfc533v27gd5lpf.apps.googleusercontent.com">
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </QueryClientProvider>
+      </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
