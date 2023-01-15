9ea753b82c27ca102b9a8cfe05e4d11b92204cf3
nahoyeon <nahy0107@naver.com>
2023-01-15 22:53:19 +0900

    feat : 유저정보 jsx 생성

--- client/src/components/userInfo/ UserInfo.jsx
+++ client/src/components/userInfo/ UserInfo.jsx
@@ -1,11 +1,32 @@
 // import React from 'react';
 // import BlackButton from './userInfoForm/BlackButton';
 import UserInfoInputBox from './userInfoForm/UserInfoInputBox';
-import { Template, UserInfoForm } from './ UserInfo.style';
+import { Template, UserInfoForm, ChangeButton } from './ UserInfo.style';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+// import ChangeButton from './userInfoForm/ChangeButton';
+import { useState } from 'react';
+import NameModal from './userInfoForm/NameModal';
+import EmailModal from './userInfoForm/EmailModal';
+import PhoneModal from './userInfoForm/PhoneModal';
+import HomeModal from './userInfoForm/HomeModal';
 const UserInfo = () => {
+  const [name, setName] = useState(false);
+  const [mail, setMail] = useState(false);
+  const [phone, setPhone] = useState(false);
+  const [home, setHome] = useState(false);
+  const nameHandler = () => {
+    setName(true);
+  };
+  const mailHandler = () => {
+    setMail(true);
+  };
+  const phoneHandler = () => {
+    setPhone(true);
+  };
+  const homeHandler = () => {
+    setHome(true);
+  };
   return (
     <>
       <Template>
@@ -14,42 +35,68 @@ const UserInfo = () => {
           <h1>개인정보</h1>
         </div>
         <UserInfoForm>
-          <UserInfoInputBox
-            width="100px"
-            labelName="실명"
-            inputId="id"
-            inputType="text"
-            name="id"
-            // onChangeInput={onChangeInput}
-            text="나호연"
-          />
-          <UserInfoInputBox
-            width="100px"
-            labelName="이메일 주소"
-            inputId="이메일"
-            inputType="email"
-            name="email"
-            // onChangeInput={onChangeInput}
-            text="nahy0107@naver.com"
-          />
-          <UserInfoInputBox
-            width="100px"
-            labelName="전화번호"
-            inputId="전화번호"
-            inputType="tel"
-            name="tel"
-            // onChangeInput={onChangeInput}
-            text="010-9775-7034"
-          />
-          <UserInfoInputBox
-            width="100px"
-            labelName="주소"
-            inputId="주소"
-            inputType="url"
-            name="url"
-            // onChangeInput={onChangeInput}
-            text="경상북도 구미시 상사서로 17 상모동 우방아파트 202동 1205호"
-          />
+          <div className="inputHead">
+            <UserInfoInputBox
+              labelName="실명"
+              inputId="id"
+              inputType="text"
+              name="id"
+              // onChangeInput={onChangeInput}
+              text="나호연"
+            />
+            <div className="changeButtonStyle">
+              <ChangeButton onClick={nameHandler}>수정</ChangeButton>
+              {name && <NameModal setName={setName} />}
+            </div>
+          </div>
+        </UserInfoForm>
+        <UserInfoForm>
+          <div className="inputHead">
+            <UserInfoInputBox
+              labelName="이메일 주소"
+              inputId="이메일"
+              inputType="email"
+              name="email"
+              // onChangeInput={onChangeInput}
+              text="nahy0107@naver.com"
+            />
+            <div className="changeButtonStyle">
+              <ChangeButton onClick={mailHandler}>수정</ChangeButton>
+              {mail && <EmailModal setMail={setMail} />}
+            </div>
+          </div>
+        </UserInfoForm>
+        <UserInfoForm>
+          <div className="inputHead">
+            <UserInfoInputBox
+              labelName="전화번호"
+              inputId="전화번호"
+              inputType="tel"
+              name="tel"
+              // onChangeInput={onChangeInput}
+              text="010-9775-7034"
+            />
+            <div className="changeButtonStyle">
+              <ChangeButton onClick={phoneHandler}>수정</ChangeButton>
+              {phone && <PhoneModal setPhone={setPhone} />}
+            </div>
+          </div>
+        </UserInfoForm>
+        <UserInfoForm>
+          <div className="inputHead">
+            <UserInfoInputBox
+              labelName="주소"
+              inputId="주소"
+              inputType="url"
+              name="url"
+              // onChangeInput={onChangeInput}
+              text="경상북도 구미시 상사서로 17 우방아파트 상모동 202동 1205호"
+            />
+            <div className="changeButtonStyle">
+              <ChangeButton onClick={homeHandler}>수정</ChangeButton>
+              {home && <HomeModal setHome={setHome} />}
+            </div>
+          </div>
         </UserInfoForm>
       </Template>
     </>
--- client/src/components/userInfo/ UserInfo.style.js
+++ client/src/components/userInfo/ UserInfo.style.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
 export const Template = styled.div`
+  border-radius: 12px;
   border: solid 1px black;
-  height: 100%;
   padding: 15px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
   h1 {
     font-size: 27px;
     margin-bottom: 40px;
@@ -17,4 +22,26 @@ export const Template = styled.div`
     }
   }
 `;
-export const UserInfoForm = styled.div``;
+export const UserInfoForm = styled.div`
+  .inputHead {
+    display: flex;
+    justify-content: space-between;
+  }
+  .changeButtonStyle {
+    margin-top: 65px;
+    margin-left: 30px;
+  }
+`;
+export const ChangeButton = styled.button`
+  width: 45px;
+  height: 40px;
+  border: solid 1px black;
+  border-radius: 12px;
+  text-decoration: underline;
+  background-color: black;
+  cursor: pointer;
+  color: white;
+  :hover {
+    background-color: darkgray;
+  }
+`;
