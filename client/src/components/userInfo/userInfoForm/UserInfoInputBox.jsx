24e711019a39eebada37bdaadd332e0eed6bcb8b
nahoyeon <nahy0107@naver.com>
2023-01-15 22:51:27 +0900

    fix : 유저정보 input 컴포넌트 수정

--- client/src/components/userInfo/userInfoForm/UserInfoInputBox.jsx
+++ client/src/components/userInfo/userInfoForm/UserInfoInputBox.jsx
@@ -1,13 +1,16 @@
 // import React from "react";'
 import { InputContainer } from './UserInfoInputBox.style';
+// import { useState } from 'react';
 
 const UserInfoInputBox = ({ labelName, inputId, inputType, name, onChangeInput, value, text }) => {
   return (
     <>
       <InputContainer>
-        <label className="inputLabel" htmlFor={inputId}>
-          {labelName}
-        </label>
+        <div className="inputHead">
+          <label className="inputLabel" htmlFor={inputId}>
+            {labelName}
+          </label>
+        </div>
         <div name={name} className="inputBox" id={inputId} type={inputType} value={value} onChange={onChangeInput}>
           {text}
         </div>
--- client/src/components/userInfo/userInfoForm/UserInfoInputBox.style.js
+++ client/src/components/userInfo/userInfoForm/UserInfoInputBox.style.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
 export const InputContainer = styled.div`
-  width: 350px;
   .inputLabel {
     height: 20px;
     display: flex;
     margin-left: 3px;
-    margin-bottom: 5px;
+    margin-bottom: 10px;
     margin-top: 50px;
     color: ${(props) => props.theme.gray};
   }
   .inputBox {
-    width: 340px;
+    width: 42vw;
     border: none;
     border-bottom: 3px solid darkgray;
     padding-left: 3px;
