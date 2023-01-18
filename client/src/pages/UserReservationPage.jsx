7f81735c54b5dc5e463dcc17b2a467abe523017a
shinker1002 <shinker1002@naver.com>
2023-01-18 21:25:58 +0900

    Feat: 사용자 예약 리스트 UI 구현

--- client/src/pages/UserReservationPage.jsx
+++ client/src/pages/UserReservationPage.jsx
@@ -1,15 +1,29 @@
-// import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 import { Container, MainContainer } from './UserReservationPage.style';
 import Header from '../components/header/Header';
-import ReservationUserEmpty from '../components/userReservation/UserReservationEmpty';
-
+// import UserReservationEmpty from '../components/userReservation/UserReservationEmpty';
+import UserReservationList from '../components/userReservation/UserReservationList';
+import { getUserReservation } from '../modules/userReservation/userReservation';
+import Pagination from '../components/pagination/Pagination';
 const ReservationUserPage = () => {
+  const [page, setpage] = useState(1);
+
+  const itemChange = (page) => {
+    setpage(page.selected + 1);
+  };
+  const { isLoading, data, isError } = useQuery('productList', async () => {
+    const data = await getUserReservation(page);
+    return data;
+  });
   return (
     <>
       <Container>
-        <Header />
+        <Header type="short" />
         <MainContainer>
-          <ReservationUserEmpty />
+          {/* <UserReservationEmpty /> */}
+          <UserReservationList data={data} isLoading={isLoading} isError={isError} />
+          <Pagination itemChange={itemChange} />
         </MainContainer>
       </Container>
     </>
