0082a5ae792fa6f6fbc3f5bd100b8f0b454aedba
shinker1002 <shinker1002@naver.com>
2023-01-19 17:59:10 +0900

    Feat: 판매자 상품 등록 리스트 UI 구현

---
+++ client/src/modules/sellerProductList/sellerProductList.js
@@ -0,0 +1,10 @@
+import axiosInstance from '..';
+
+export const getSellerProductList = (page) => {
+  let params = {
+    page,
+  };
+  console.log(params);
+  console.log('판매자 상품 리스트 Loading');
+  return axiosInstance.get('/productMy');
+};
---
+++ client/src/pages/SellerProductListPage.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Container, MainContainer } from './SellerProductListPage.style';
+import Header from '../components/header/Header';
+import SellerProductList from '../components/sellerProductList/SellerProductList';
+import { getSellerProductList } from '../modules/sellerProductList/sellerProductList';
+import Pagination from '../components/pagination/Pagination';
+
+const SellerProductListPage = () => {
+  const [page, setpage] = useState(1);
+
+  const itemChange = (page) => {
+    setpage(page.selected + 1);
+  };
+  const { isLoading, data, isError } = useQuery('sellerProductList', async () => {
+    const data = await getSellerProductList(page);
+    return data;
+  });
+  return (
+    <>
+      <Container>
+        <Header type="short" />
+        <MainContainer>
+          <SellerProductList data={data} isLoading={isLoading} isError={isError} />
+          <Pagination itemChange={itemChange} />
+        </MainContainer>
+      </Container>
+    </>
+  );
+};
+export default SellerProductListPage;
