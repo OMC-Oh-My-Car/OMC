e0cb459624777b53e8e44099f34480ee822b9a76
Taekgil99 <taekgil273@gmail.com>
2023-01-19 09:17:36 +0900

    Feat: GlobalExceptionHandler에 웹훅 메세지 전송 추가 #37

--- server/src/main/java/com/omc/global/error/GlobalExceptionHandler.java
+++ server/src/main/java/com/omc/global/error/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.omc.global.error;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -9,20 +11,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.omc.global.error.exception.BusinessException;
+import com.omc.global.webhook.WebhookService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+	private final WebhookService webhookService;
 
 	/**
 	 * javax.validation.Valid 또는 @Validated binding error가 발생할 경우
 	 */
 	@ExceptionHandler(BindException.class)
-	protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+	protected ResponseEntity<ErrorResponse> handleBindException(BindException e) throws IOException {
 		log.error("handleBindException", e);
 		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getBindingResult());
+		webhookService.sendErrorWebhook(e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 							 .body(errorResponse);
 	}
@@ -32,9 +39,10 @@ public class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
 	protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
-		MethodArgumentTypeMismatchException e) {
+		MethodArgumentTypeMismatchException e) throws IOException {
 		log.error("handleMethodArgumentTypeMismatchException", e);
 		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+		webhookService.sendErrorWebhook(e);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 							 .body(errorResponse);
 	}
@@ -44,9 +52,10 @@ public class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
-		HttpRequestMethodNotSupportedException e) {
+		HttpRequestMethodNotSupportedException e) throws IOException {
 		log.error("handleHttpRequestMethodNotSupportedException", e);
 		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED.toString(), e.getMessage());
+		webhookService.sendErrorWebhook(e);
 		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
 	}
 
@@ -54,9 +63,10 @@ public class GlobalExceptionHandler {
 	 * 비즈니스 로직 실행 중 오류 발생
 	 */
 	@ExceptionHandler(value = {BusinessException.class})
-	protected ResponseEntity<ErrorResponse> handleConflict(BusinessException e) {
+	protected ResponseEntity<ErrorResponse> handleConflict(BusinessException e) throws IOException {
 		log.error("BusinessException", e);
 		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getErrorCode(), e.getMessage());
+		webhookService.sendErrorWebhook(e);
 		return ResponseEntity.status(e.getErrorCode().getHttpStatus())
 							 .body(errorResponse);
 	}
@@ -65,9 +75,10 @@ public class GlobalExceptionHandler {
 	 * 나머지 예외 발생
 	 */
 	@ExceptionHandler(Exception.class)
-	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+	protected ResponseEntity<ErrorResponse> handleException(Exception e) throws IOException {
 		log.error("Exception", e);
 		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage());
+		webhookService.sendErrorWebhook(e);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
 
