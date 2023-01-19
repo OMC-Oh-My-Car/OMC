9abbb1d26a98f61a97df68f925013e736b7c8abe
shinker1002 <shinker1002@naver.com>
2023-01-20 05:24:55 +0900

    Feat: 상품 예약 페이지 UI

---
+++ client/src/pages/SellerReservationPage.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Container, MainContainer } from './SellerReservationPage.style';
+import Header from '../components/header/Header';
+import Pagination from '../components/pagination/Pagination';
+import SellerReservationList from '../components/sellerReservation/SellerReservationList';
+import { getSellerReservationList } from '../modules/sellerReservation/sellerReservation';
+const SellerReservationPage = () => {
+  const [page, setpage] = useState(1);
+  const [filter, setFilter] = useState(0);
+
+  const itemChange = (page) => {
+    setpage(page.selected + 1);
+  };
+
+  const { isLoading, data, isError } = useQuery('sellerProductList', async () => {
+    const data = await getSellerReservationList(filter, page);
+    return data;
+  });
+
+  return (
+    <>
+      <Container>
+        <Header type="short" />
+        <MainContainer>
+          <SellerReservationList
+            data={data}
+            isLoading={isLoading}
+            isError={isError}
+            filter={filter}
+            setFilter={setFilter}
+          />
+          <Pagination itemChange={itemChange} />
+        </MainContainer>
+      </Container>
+    </>
+  );
+};
+export default SellerReservationPage;
