2e9ea755f0a3ca3ae14ec873fe53555c2495fddb
shinker1002 <shinker1002@naver.com>
2023-01-20 02:31:06 +0900

    Feat: 판매자 상품 예약 리스트 스켈레톤 컴포넌트 UI

---
+++ client/src/components/sellerReservation/SellerReservationItemSkeleton.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+import { SellerReservationItemSkeletonArea } from './SellerReservationItemSkeleton.style';
+
+const SellerReservationItemSkeleton = () => {
+  return (
+    <>
+      <SellerReservationItemSkeletonArea>
+        <div className="image"></div>
+        <div className="flexRight">
+          <div className="reservationInfo">
+            <span className="reservationNumber"></span>
+            <span className="reservationDate"></span>
+            <span className="userEmail"></span>
+          </div>
+          <div className="button">
+            <div className="buttonLeft" />
+            <div className="buttonRight" />
+          </div>
+        </div>
+      </SellerReservationItemSkeletonArea>
+    </>
+  );
+};
+
+export default SellerReservationItemSkeleton;
---
+++ client/src/components/sellerReservation/SellerReservationItemSkeleton.style.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const SellerReservationItemSkeletonArea = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 20px;
+  .image {
+    width: 200px;
+    height: 170px;
+    background-color: #eee;
+    margin-right: 20px;
+  }
+  .flexRight {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-grow: 1;
+    height: 100%;
+    .reservationInfo {
+      display: flex;
+      flex-direction: column;
+      .reservationNumber {
+        width: 100%;
+        max-width: 200px;
+        height: 25px;
+        background-color: #eee;
+        margin-bottom: 15px;
+      }
+      .reservationDate {
+        width: 100%;
+        max-width: 300px;
+        height: 25px;
+        background-color: #eee;
+        margin-bottom: 10px;
+      }
+      .userEmail {
+        width: 100%;
+        max-width: 300px;
+        height: 25px;
+        background-color: #eee;
+      }
+    }
+    .button {
+      display: flex;
+      justify-content: flex-end;
+      width: 100%;
+      .buttonLeft {
+        width: 105px;
+        height: 42px;
+        border-radius: 5px;
+        background-color: #eee;
+        margin-right: 10px;
+      }
+      .buttonRight {
+        width: 105px;
+        height: 42px;
+        border-radius: 5px;
+        background-color: #eee;
+      }
+    }
+  }
+`;
