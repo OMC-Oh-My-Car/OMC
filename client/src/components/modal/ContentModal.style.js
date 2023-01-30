104a2c6165822aff8ee9f07b79691a8a9d21f7fe
shinker1002 <shinker1002@naver.com>
2023-01-30 21:33:23 +0900

    Feat: 디테일 페이지 내용 모달 추가

---
+++ client/src/components/modal/ContentModal.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ContentModalArea = styled.div`
+  width: 100%;
+  height: 90%;
+  overflow-y: scroll;
+  flex-direction: column;
+  padding-right: 25px;
+  h2 {
+    font-size: 24px;
+    font-weight: 700;
+    margin-bottom: 30px;
+  }
+  span {
+    width: 100%;
+    font-weight: 300;
+    line-height: 24px;
+    word-break: break-word;
+  }
+`;
