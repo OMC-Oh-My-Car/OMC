d006c2f21287ce66209d8d306c977844c239ca1e
shinker1002 <shinker1002@naver.com>
2023-02-16 06:26:14 +0900

    Feat: 판매자 상품 등록 API 연결

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -5,10 +5,10 @@ import axios from 'axios';
 console.log(process.env.REACT_APP_API_URL);
 const axiosInstance = axios.create({
   headers: {
-    'Access-Control-Allow-Origin': 'https://c683-220-123-236-194.jp.ngrok.io',
+    'Access-Control-Allow-Origin': 'https://5c97-220-123-236-194.jp.ngrok.io',
     'ngrok-skip-browser-warning': '69420',
     'Access-Control-Allow-Headers': '*',
-    'Content-Type': 'application/json',
+    // 'Content-Type': 'application/json',
     // 등등의 header
   },
   baseURL: process.env.REACT_APP_API_URL, // baseUrl
--- client/src/modules/sellerProduct/sellerProductAdd.js
+++ client/src/modules/sellerProduct/sellerProductAdd.js
@@ -1,10 +1,31 @@
 import axiosInstance from '..';
 
+axiosInstance.defaults.headers.common['Authorization'] = window.sessionStorage.getItem('Authorization');
+axiosInstance.defaults.headers.common['Content-Type'] = 'multipart/form-data';
+
 export const createNewProduct = (content) => {
-  let params = {
-    content,
-  };
-  console.log(params);
+  console.log(window.sessionStorage.getItem('Authorization'));
+  let frm = new FormData();
+  const product = JSON.stringify({
+    subject: content.subject,
+    description: content.description,
+    address: content.address,
+    zipcode: content.zipcode,
+    facilities: content.facilities,
+    telephone: content.telephone,
+    price: content.price,
+    checkIn: content.checkIn,
+    checkOut: content.checkOut,
+  });
+
+  const productBlob = new Blob([product], { type: 'application/json' });
+  frm.append('product', productBlob);
+
+  for (let i = 0; i < content.image.length; i++) {
+    frm.append('imgUrl', content.image[i]);
+  }
+
+  console.log(content.image[0].name);
   console.log('판매자 상품 등록');
-  return axiosInstance.post('/product');
+  return axiosInstance.post('/product', frm);
 };
