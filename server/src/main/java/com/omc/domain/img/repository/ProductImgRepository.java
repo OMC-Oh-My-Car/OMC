6c5039ba49b84d01688a06998a5b163aaa146a08
Taekgil99 <taekgil273@gmail.com>
2023-02-16 14:18:22 +0900

    Feat: ProductImgRepository 추가

---
+++ server/src/main/java/com/omc/domain/img/repository/ProductImgRepository.java
@@ -0,0 +1,16 @@
+package com.omc.domain.img.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.omc.domain.img.entity.ProductImg;
+
+public interface ProductImgRepository extends JpaRepository<ProductImg, Long> {
+
+	void deleteByProductId(Long productId);
+
+	List<ProductImg> findAllByProductId(Long productId);
+
+	ProductImg findFirstByProductId(Long productId);
+}
