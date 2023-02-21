016b7c6b14979cbc9a7846fc9a68855ab2ab600d
shinker1002 <shinker1002@naver.com>
2023-02-21 17:20:52 +0900

    Feat: 헤더 네비게이션 연결

--- client/src/components/header/nav/user/userInfo/UserInfoDetail.jsx
+++ client/src/components/header/nav/user/userInfo/UserInfoDetail.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useRef } from 'react';
 import { UserInfoDetailArea } from './UserInfoDetail.style';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../../../../modules/member/logout';
+import { clearUserInfo } from '../../../../../redux/slice/UserInfo';
+import { useDispatch } from 'react-redux';
 
 const UserInfoDetail = ({ toggleSide }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const outSide = useRef();
 
   useEffect(() => {
@@ -10,6 +16,21 @@ const UserInfoDetail = ({ toggleSide }) => {
       document.removeEventListener('mousedown', handlerOutside);
     };
   });
+
+  const handleLogout = async () => {
+    logout()
+      .then((res) => {
+        console.log('로그아웃되었습니다.');
+        window.sessionStorage.removeItem('Authorization');
+        dispatch(clearUserInfo());
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(`${err.response.status} 에러`);
+        console.log('로그아웃 실패!');
+      });
+  };
+
   const handlerOutside = (e) => {
     if (!outSide.current.contains(e.target)) {
       toggleSide();
@@ -20,12 +41,21 @@ const UserInfoDetail = ({ toggleSide }) => {
     <>
       <UserInfoDetailArea ref={outSide}>
         <ul className="menu">
-          <li className="menuItem">회원 정보</li>
-          <li className="menuItem">회원 정보 수정</li>
-          <li className="menuItem">찜 목록</li>
+          <li role="presentation" onClick={() => navigate('/user/:id/userInfo')} className="menuItem">
+            회원 정보
+          </li>
+          <li role="presentation" onClick={() => navigate('/user/:id/userInfo/edit')} className="menuItem">
+            회원 정보 수정
+          </li>
+          {/* <li className="menuItem">찜 목록</li> */}
+          <li role="presentation" onClick={() => navigate('/user/12/reservation')} className="menuItem">
+            예약 목록
+          </li>
         </ul>
         <ul className="menu">
-          <li className="menuItem">로그아웃</li>
+          <li role="presentation" onClick={handleLogout} className="menuItem">
+            로그아웃
+          </li>
         </ul>
       </UserInfoDetailArea>
     </>
